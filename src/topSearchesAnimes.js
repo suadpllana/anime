0fefd2442c6128411cea5e7e56c3af595ea64a9f
suadpllana <suadpllana12@gmail.com>
2025-05-12 01:45:35 +0200

    first commit

--- src/topSearchesAnimes.js
+++ src/topSearchesAnimes.js
@@ -1,12 +1,12 @@
 export const topSearchedAnime = [
-  "Solo Leveling Season 2: Arise from the Shadow",
-  "The Apothecary Diaries Season 2",
-  "Demon Slayer",
-  "My Happy Marriage Season 2",
   "One Piece",
-  "Dr. Stone: Science Future",
-  "Hunter x Hunter",
-  "Honey Lemon Soda",
-  "Medaka Kuroiwa Is Impervious to My Charms",
-  "I Have a Crush at Work"
-]
+  "Demon Slayer: Kimetsu no Yaiba",
+  "Solo Leveling",
+  "Dragon Ball Daima",
+  "Frieren: Beyond Journey’s End",
+  "My Hero Academia: Vigilantes",
+  "Sakamoto Days",
+  "The Apothecary Diaries",
+  "Dan Da Dan",
+  "Kaiju No. 8"
+];
